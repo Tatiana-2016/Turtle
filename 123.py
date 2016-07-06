3bb5af78c3a359356c1696bb2acf0ffcde06b24a
Татьяна <beteht-te@yandex.ru>
2016-07-06 19:28:45 +0400

    черепашка нарисовала 6-угольник

--- 123.py
+++ 123.py
@@ -1,12 +1,12 @@
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
-t.color("darkgreen", "yellow")
+t.color("red", "black")
 t.shapesize(1)
+t.width(5)
 
-
-for x in range(4):
+for x in range(6):
     t.forward(200)
-    t.left(90)
+    t.left(60)
 
-time.sleep(10)
+#time.sleep(10)
