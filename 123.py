2626282006be8977c478a460ba666b7c019f26aa
Татьяна <beteht-te@yandex.ru>
2016-07-07 16:02:48 +0400

    Разобралась с задержкой. Рисует 6-угольник и зависает на 10 секунд

--- 123.py
+++ 123.py
@@ -1,12 +1,11 @@
 import turtle
+import time
 t = turtle.Turtle()
-t.shape("turtle")
-t.color("red", "black")
-t.shapesize(1)
+t.color("red")
 t.width(5)
 
 for x in range(6):
-    t.forward(200)
-    t.left(60)
-#ghghghgh
-#time.sleep(10)
+     t.forward(200)
+     t.left(60)
+
+time.sleep(10)
